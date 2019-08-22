5c990e089f8a68245c5540ab24965be75987f870
ELAINE WIJAYA <elainewijaya@gmail.com>
2019-08-22 12:41:44 +0800

    Updated comments for 3 input version

--- ThreeInputs.java
+++ ThreeInputs.java
@@ -18,23 +18,30 @@ public class ThreeInputs {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // Initialise our list of size 3
         Float[] list = new Float[3];
+        // Initialise sum
         Float sum = 0f;
         
         Scanner input = new Scanner(System.in);
+            // Loop 3 times
             for (int i = 0; i < list.length; i++) {
                 System.out.println("Enter the next number:");
+                // Get the next input
                 list[i] = input.nextFloat();
                 sum += list[i];
             }
     System.out.println("You have entered the following numbers: " + Arrays.toString(list));
     
+    // Sort the list in ascending order
     Arrays.sort(list);
-    System.out.println("Max:  " + list[0]);
-    System.out.println("Min:  " + list[list.length-1]);
+    // Min is the first element
+    System.out.println("Min:  " + list[0]);
+    // Max is the last number
+    System.out.println("Max:  " + list[list.length-1]);
+    // Average is the sum divided by the number of elements in the list
     System.out.println("Average:  " + sum/list.length);
     }
-    // TODO code application logic here
         
 
     
